d8392f0a6d9fe9cfb248bb99830d2e88862999f8
David Nasrulloh <davidkrb52@gmail.com>
2023-08-07 23:41:13 +0700

    feat/david: add display random number and game button control

--- Section 3 Mini Game/mini-game/components/ui/Title.jsx
+++ Section 3 Mini Game/mini-game/components/ui/Title.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { StyleSheet, Text } from 'react-native'
-import Colors from '../constants/colors'
+import Colors from '../../constants/colors'
 
 const Title = ({children}) => {
     return (
